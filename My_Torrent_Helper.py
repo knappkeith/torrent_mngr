c26795bb2256b93d07a50bb743b4e1af450c8727
Keith Knapp <knapp.keith@gmail.com>
2015-05-13 12:40:13 -0600

    fixing things

--- My_Torrent_Helper.py
+++ My_Torrent_Helper.py
@@ -3,13 +3,14 @@ from rar_handler import My_Rar
 import os
 import sys
 from keith.file_mngr import My_Files
+from tv_shows import TV_Shows
 
 # Define Variables
 save_path = '~/torrents/extracted'
 base_path = '~/torrents/completed'
 video_ext = ['.avi', '.mkv', '.mp4', '.m4v', '.mpeg']
-# network_path = '/Volumes/Public/Shared Videos/TV Shows'
 network_path = '/Volumes/Public/Shared Videos/TV Shows'
+# network_path = '/Users/keith/torrents/test_network'
 
 # Initialize Needed Classes
 my_rar = My_Rar(base_path, save_path)
@@ -28,24 +29,24 @@ my_rar.get_total_size()
 # Check for rars to Extract
 if not my_rar.rar_objs:
 	print 'Nothing to Extract, all up-to-date'
-	sys.exit()
+else:
 
-# Print out what needs to be done
-print ""
-print "The following needs to be extracted:"
-for i in my_rar.rar_ext_names:
-	print i
-print ""
-print "A total of " + str(len(my_rar.rar_objs)) + " file(s) and " + str(my_rar.total_size/1024/1024) + " Mb."
+	# Print out what needs to be done
+	print ""
+	print "The following needs to be extracted:"
+	for i in my_rar.rar_ext_names:
+		print i
+	print ""
+	print "A total of " + str(len(my_rar.rar_objs)) + " file(s) and " + str(my_rar.total_size/1024/1024) + " Mb."
 
-# Ask to continue
-answer = raw_input('Would you like to continue(Y/N)?:')
-if not (answer.upper() == 'Y' or answer.upper() == 'YES'):
-	print 'nevermind'
-	sys.exit()
+	# Ask to continue
+	answer = raw_input('Would you like to continue(Y/N)?:')
+	if not (answer.upper() == 'Y' or answer.upper() == 'YES'):
+		print 'nevermind'
+		sys.exit()
 
-# Extract ALL THE THINGS
-my_rar.extract_all()
+	# Extract ALL THE THINGS
+	my_rar.extract_all()
 
 # Check for Network Path
 if not os.path.isdir(network_path):
@@ -58,13 +59,15 @@ if not (answer.upper() == 'Y' or answer.upper() == 'YES'):
 	print 'nevermind'
 	sys.exit()
 
-# Get all of files from network drive
-my_network = My_Files(network_path)
-
-# Filter all Network files for only video files
+# Get all of the TV Shows from both directories
+a = TV_Shows(save_path)
+b = TV_Shows(network_path)
 
 # Determine which files need to be copied
+a.determine_copy(b.show_directory)
 
-# Determine where to copy files
-
-# Copy Files
+# print each file to copy
+for show in a.show_directory:
+	for season in a.show_directory[show]:
+		for episode in a.show_directory[show][season]:
+			print 'Episode %s in season %s of %s needs to be copied' % (episode, season, show)
--- tv_shows.py
+++ tv_shows.py
@@ -5,6 +5,7 @@
 import os
 from guessit import guess_file_info
 from keith.file_mngr import My_Files
+import copy
 
 class TV_Shows(object):
 	def __init__(self, path):
@@ -20,6 +21,8 @@ class TV_Shows(object):
 				self.tv_files = list(path)
 		else:
 			raise NameError('Invalid File Path')
+		self.get_all_show_info()
+		self.build_tv_show_directory()
 
 	def get_all_show_info(self):
 		self.tv_infos = {}
@@ -28,30 +31,59 @@ class TV_Shows(object):
 
 	def parse_desired_info(self, show_info):
 		my_info = {}
+		gtg = True
 		if show_info['type'] != 'episode':
-			print 'Not a TV Show'
-			return None
+			my_info['show'] = '??'
+			gtg = False
 		if not 'series' in show_info:
-			raise NameError("Show cannot be determined.")
+			my_info['show'] = '??'
+			gtg = False
 		else:
 			my_info['show'] = show_info['series']
 		if not 'season' in show_info:
 			if not 'seasonList' in show_info:
-				raise NameError("Season cannot be determined.")
+				my_info['season'] = '??'
+				gtg = False
 			else:
 				my_info['season'] = show_info['seasonList'][0]
 		else:
 			my_info['season'] = show_info['season']
 		if not 'episodeNumber' in show_info:
 			if not 'seasonList' in show_info:
-				raise NameError("Episode cannot be determined.")
+				my_info['episode'] = '??'
+				gtg = False
 			else:
 				my_info['episode'] = show_info['episodeList'][0]
 		else:
 			my_info['episode'] = show_info['episodeNumber']
-		return my_info
+		return my_info, gtg
 
+
+	def build_tv_show_directory(self):
+		self.show_directory = {}
+		for i in self.tv_infos:
+
+			# Ensure that te correct keys are available
+			tv_info, parsed = self.parse_desired_info(self.tv_infos[i])
+			if parsed == None:
+				self._add_show_to_directory(tv_info['show'])
+				self._add_season_to_directory(tv_info['show'], tv_info['season'])
+				self._add_episode_to_directory(tv_info['show'], tv_info['season'], tv_info['episode'], i)
+			else:
+				print "TV01: Unable to parse %s, skipping! (%s, %s, %s)" % (i, tv_info['show'], tv_info['season'], tv_info['episode'])
 	
+	def determine_copy(self, slave_directory):
+		my_show_directory = copy.deepcopy(self.show_directory)
+		my_slave_directory = copy.deepcopy(slave_directory)
+		for show in my_show_directory:
+			if show in my_slave_directory:
+				for season in my_show_directory[show]:
+					if season in my_slave_directory[show]:
+						for episode in my_show_directory[show][season]:
+							if episode in my_slave_directory[show][season]:
+								del self.show_directory[show][season][episode]
+
+
 	def _add_show_to_directory(self, show_name):
 		if not show_name in self.show_directory:
 			self.show_directory[show_name] = {}
@@ -62,18 +94,6 @@ class TV_Shows(object):
 			self.show_directory[show_name][season_num] = {}
 
 
-	def build_tv_show_directory(self):
-		self.show_directory = {}
-		for i in self.tv_infos:
-
-			# Ensure that te correct keys are available
-			tv_info = self.parse_desired_info(self.tv_infos[i])
-			if not tv_info == None:
-				self._add_show_to_directory(tv_info['show'])
-				self._add_season_to_directory(tv_info['show'], tv_info['season'])
-				self._add_episode_to_directory(tv_info['show'], tv_info['season'], tv_info['episode'], i)
-	
-
 	def _add_episode_to_directory(self, show_name, season_num, episode_num, path):
 		if type(episode_num) == 'list':
 			episodes = list(episode_num)
@@ -83,7 +103,7 @@ class TV_Shows(object):
 			if not episode in self.show_directory[show_name][season_num]:
 				self.show_directory[show_name][season_num][episode] = path
 			else:
-				print 'Episode %s already exists for Season %s of %s.' % (episode, season_num, show_name)
+				print 'TV02: Episode %s already exists for Season %s of %s.' % (episode, season_num, show_name)
 
 
 
